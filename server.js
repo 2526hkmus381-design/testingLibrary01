b5033a9cb059896341012370fffb5ecf924266ed
2526hkmus381-design <2526hkmus381@gmail.com>
2025-10-17 19:18:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ async function initializeUsers() {
 //});
 
 // Server login page
-app.post('/login', async (req, res) => {
+app.post('/', async (req, res) => {
   const{username, password}= req.body;
 //  if(!db){
  //   return res.status(500).send("Database not connected");
